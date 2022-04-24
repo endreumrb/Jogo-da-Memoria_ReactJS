ea54052abc9aa1bbff5cd9b4b7e4a9458dc26d2b
Endreu Benites <endreu.matteusrb@gmail.com>
2022-04-23 23:28:46 -0300

    Criando estrutura básica

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+
 const App = () => {
-  return <div>...</div>;
+  return (
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href=''>
+          <img src={logoImage} width='200' alt='' />
+        </C.LogoLink>
+        <C.InfoArea>...</C.InfoArea>
+        <button type='button'>Reiniciar</button>
+      </C.Info>
+      <C.GridArea></C.GridArea>
+    </C.Container>
+  );
 };
 
 export default App;
